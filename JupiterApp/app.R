337017c2f5f50db1fca122c6a564aa8e464cada6
cgmariani <chantos.mariani@outlook.com>
2019-05-08 18:18:24 -0300

    Third commit.

--- JupiterApp/app.R
+++ JupiterApp/app.R
@@ -1,19 +1,24 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
 #
-# Find out more about building applications with Shiny here:
+# Shiny Apps
 #
-#    http://shiny.rstudio.com/
 #
 
+# Deploying to Shinnyapps.io
+path <- "C:/Users/User Ambev/Desktop/Stats/HelloWorld/HelloJupiter/JupiterApp"
+install.packages("rsconnect")
+library(rsconnect)
 library(shiny)
+rsconnect::setAccountInfo(name='cgmariani',
+                          token='4645870327DD7ABF66E492999A259754',
+                          secret='2q3uTWpOF3uY5PvZTpMr4YQlre8Lu/33cw9Tn2U5')
+rsconnect::deployApp(path)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Jupiter App"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -22,7 +27,7 @@ ui <- fluidPage(
                         "Number of bins:",
                         min = 1,
                         max = 50,
-                        value = 30)
+                        value = 25)
         ),
 
         # Show a plot of the generated distribution
@@ -41,7 +46,7 @@ server <- function(input, output) {
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        hist(x, breaks = bins, col = 'skyblue2', border = 'white')
     })
 }
 
